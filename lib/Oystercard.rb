21a5a1c1bb41f872aa885bfeec5a10f51a6d5c4f
Jack Gumoes <j.gumoes@gmail.com>
2021-02-08 17:03:46 +0000

    touch_in requires a station ID, oystercard saves entry station ID on touch in, deletes station ID on touch out. also, re-structered Oystercard_spec.rb

--- lib/Oystercard.rb
+++ lib/Oystercard.rb
@@ -3,12 +3,11 @@ class Oystercard
   BALANCE_LIMIT = 90
   MINIMUM_FARE = 1
 
-  attr_reader :balance, :in_journey
+  attr_reader :balance, :entry_station
 
   def initialize
     @balance = 0
     @limit = BALANCE_LIMIT
-    @in_journey = false
   end
 
   def top_up amount
@@ -17,18 +16,18 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    entry_station != nil
   end
 
-  def touch_in
+  def touch_in station_id
     raise "Insufficient funds" if @balance < MINIMUM_FARE
-    @in_journey = true
+    @entry_station = station_id
     "Touched in"
   end
 
   def touch_out
     deduct MINIMUM_FARE
-    @in_journey = false
+    @entry_station = nil
     "Touched out"
   end
 
--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -3,31 +3,19 @@ require "Oystercard.rb"
 describe Oystercard do
   MINIMUM_FARE = Oystercard::MINIMUM_FARE
   limit = Oystercard::BALANCE_LIMIT
-
-  it 'new instance of oystercard shoud have balance 0' do
-    expect(subject.balance).to eq 0
+  let(:waterloo) { double :station }
+  before (:each) do
+    allow(waterloo).to receive(:ID?).and_return(:WATERLOO)
+    subject.instance_variable_set(:@balance, MINIMUM_FARE + 1)
   end
 
   it 'oystercard can be topped up' do
-    subject.top_up(3.50)
-    expect(subject.balance).to eq 3.50
+    expect { subject.top_up(3.50) }.to change {subject.balance}.by(3.50)
   end
-
-  it "oystercard can't be topped up above the limit" do
-    subject.top_up(limit)
-    expect { subject.top_up(1) }.to raise_error "Reached Limit of #{limit}"
-  end
-
-  # it "money can be deducted from oystercard" do
-  #   subject.instance_variable_set(:@balance, 20)
-  #   subject.send(:deduct, 1.60)
-  #   expect(subject.balance).to eq 20 - 1.6
-  # end
 
   it 'oystercard can touch in' do
-    subject.instance_variable_set(:@balance, MINIMUM_FARE + 10)
     expect(subject.in_journey?).to eq false
-    expect(subject.touch_in).to eq "Touched in"
+    expect(subject.touch_in(waterloo.ID?)).to eq "Touched in"
     expect(subject.in_journey?).to eq true
   end
 
@@ -37,14 +25,41 @@ describe Oystercard do
     expect(subject.in_journey?).to eq false
   end
 
-  it "oystercard can't touch in with insufficient funds" do
-    expect { subject.touch_in }.to raise_error "Insufficient funds"
-  end
-
   it 'oystercard balance should reduce by minimum fare' do
-    subject.instance_variable_set(:@balance, MINIMUM_FARE + 10)
     subject.instance_variable_set(:@in_journey, true)
     expect { subject.touch_out }.to change{subject.balance}.by(-MINIMUM_FARE)
   end
 
+  context "oystercard starts with no money" do
+    before(:each) do
+      subject.instance_variable_set(:@balance, 0)
+    end
+
+    it 'new instance of oystercard shoud have balance 0' do
+      expect(Oystercard.new().balance).to eq 0
+    end
+
+    it "oystercard can't be topped up above the limit" do
+      subject.top_up(limit)
+      expect { subject.top_up(1) }.to raise_error "Reached Limit of #{limit}"
+    end
+
+    it "oystercard can't touch in with insufficient funds" do
+      expect { subject.touch_in(waterloo.ID?) }.to raise_error "Insufficient funds"
+    end
+  end
+
+  context "remembering stations" do
+
+    it "remembers the entry station" do
+      subject.touch_in(waterloo.ID?)
+      expect(subject.entry_station).to eq waterloo.ID?
+    end
+
+    it "forgets the entry station after touching out" do
+      subject.touch_out
+      expect(subject.entry_station).to eq nil
+    end
+
+  end
 end
