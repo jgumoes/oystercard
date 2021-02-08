cfb45526a09658e2408b345c1ab4cc8239df0044
Jack Gumoes <j.gumoes@gmail.com>
2021-02-08 15:09:26 +0000

    added top_up test to oystercard_spec.rb, added top_up method to oystercard.rb

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq 0
   end
 
+  it 'oystercard can be topped up' do
+    subject.top_up(3.50)
+    expect(subject.balance).to eq 3.50
+  end
+
 end
